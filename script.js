4586440ef54f223c4e0d484bc731145f02faa4e6
raintype13 <a78057477@gmail.com>
2025-06-23 14:47:46 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const tg = window.Telegram.WebApp;
-tg.expand();
-
-const user = tg.initDataUnsafe.user;
-document.getElementById('user-info').textContent = `👤 Welcome, ${user?.first_name || " "}!`;
-
-document.getElementById('connectWallet').onclick = () => {
-  alert("🔐 Здесь будет подключение TON Wallet через TonConnect");
-};
